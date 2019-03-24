65f3a0ba6f72b3ad396ebb65429b7bb2b8a2375e
Siddhartha Ghosh <siddhartha.ghosh.in@gmail.com>
2019-03-24 14:46:37 +0000

    Added TimeSlot split functionality and Hamcrest dependencies.

--- src/main/java/org/kritiniyoga/karmayoga/TimeSlot.java
+++ src/main/java/org/kritiniyoga/karmayoga/TimeSlot.java
@@ -1,20 +1,28 @@
 package org.kritiniyoga.karmayoga;
 
+import lombok.NonNull;
 import lombok.Value;
 
 import java.time.Instant;
+import java.util.List;
 
 @Value
 public class TimeSlot {
     Instant start, end;
 
-    public TimeSlot(Instant start, Instant end) {
-        super();
+    public TimeSlot(@NonNull Instant start, @NonNull Instant end) {
         if ( start.isAfter(end) ) {
             throw new IllegalArgumentException("End must be after start");
         } else {
-            this.end = end;
             this.start = start;
+            this.end = end;
         }
+    }
+
+    public List<TimeSlot> split(Instant splitPoint) {
+        return List.of(
+            new TimeSlot(this.start, splitPoint),
+            new TimeSlot(splitPoint, this.end)
+        );
     }
 }
--- src/test/java/org/kritiniyoga/karmayoga/TimeSlotBasics.java
+++ src/test/java/org/kritiniyoga/karmayoga/TimeSlotBasics.java
@@ -1,17 +1,23 @@
 package org.kritiniyoga.karmayoga;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TimeSlotBasics {
 
     private Instant now, future;
+    private TimeSlot timeSlot;
+    private Instant halfSplit;
 
 
     @BeforeEach
@@ -19,21 +25,38 @@ public class TimeSlotBasics {
         now = Instant.now();
         future = now.plus(2,
             ChronoUnit.HOURS);
+        timeSlot = new TimeSlot(now, future);
+        halfSplit = now.plus(1, ChronoUnit.HOURS);
     }
 
     @Test
-    public void shouldBeAbleToCreateASlot() {
-        TimeSlot timeSlot = new TimeSlot(now, future);
+    public void withStartAndEnd_shouldCreateTimeSlot() {
         assertNotNull(timeSlot);
     }
 
     @Test
-    public void shouldThrowExceptionWhenEndIsBeforeFuture() {
-
-        Assertions.assertThrows(
-            IllegalArgumentException.class,
+    public void whenEndIsBeforeFuture_shouldThrowException() {
+        assertThrows( IllegalArgumentException.class,
             () -> new TimeSlot(future, now));
-
     }
 
+    @Test
+    public void givenAnInstanceWithin_shouldBeAbleToSpitASlot() {
+        List<TimeSlot> splitSlots = timeSlot.split(halfSplit);
+        assertThat(splitSlots, hasSize(2));
+    }
+
+    @Test
+    public void whenSplittingSlots_shouldGenerateValidSplits() {
+        List<TimeSlot> splitSlots = timeSlot.split(halfSplit);
+        assertEquals(now, splitSlots.get(0).getStart());
+        assertEquals(future, splitSlots.get(1).getEnd());
+    }
+
+    @Test
+    public void givenAnInstanceOutside_whenSplittingSlots_shouldThrowException() {
+        Instant outlyingSplitPoint = now.minus(3, ChronoUnit.HOURS);
+        assertThrows(IllegalArgumentException.class,
+            () -> timeSlot.split(outlyingSplitPoint));
+    }
 }
