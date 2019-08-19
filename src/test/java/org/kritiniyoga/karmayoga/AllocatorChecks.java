86bf25d90630d82867a0f0b0175b0c5bb373c8cf
Siddhartha Ghosh <sighosh2@users.noreply.github.com>
2019-08-20 00:45:09 +0530

    * Add parameterized tests

--- src/test/java/org/kritiniyoga/karmayoga/AllocatorChecks.java
+++ src/test/java/org/kritiniyoga/karmayoga/AllocatorChecks.java
@@ -2,28 +2,28 @@ package org.kritiniyoga.karmayoga;
 
 import io.vavr.collection.List;
 import io.vavr.collection.Seq;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.kritiniyoga.karmayoga.allocators.AnotherSampleAllocator;
+import org.kritiniyoga.karmayoga.allocators.SampleAllocator;
 
 import java.time.Duration;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-public class SampleAllocatorChecks {
-    private Allocator allocator;
+public class AllocatorChecks {
 
-    @BeforeEach
-    void init() {
-        allocator = new AnotherSampleAllocator();
+    static Stream<Allocator> generateAllocators() {
+        return Stream.of(new AnotherSampleAllocator());
     }
 
-    @Test
-    public void givenTasksFittingSlots_whenAllocating_shouldAllocate() {
-
+    @ParameterizedTest
+    @MethodSource("generateAllocators")
+    public void givenTasksFittingSlots_whenAllocating_shouldAllocate(Allocator allocator) {
         Task aTask = Task.builder().estimate(Duration.ofHours(2)).build();
         Instant start = Instant.now().plus(2, ChronoUnit.HOURS);
         Instant end = Instant.now().plus(4, ChronoUnit.HOURS);
@@ -34,8 +34,9 @@ public class SampleAllocatorChecks {
         assertThat(schedules.get().getTask()).isEqualTo(aTask);
     }
 
-    @Test
-    public void givenTasksNotFittingSlots_whenAllocating_shouldNotAllocate() {
+    @ParameterizedTest
+    @MethodSource("generateAllocators")
+    public void givenTasksNotFittingSlots_whenAllocating_shouldNotAllocate(Allocator allocator) {
         Task aTask = Task.builder().estimate(Duration.ofHours(5)).build();
         Instant start = Instant.now().plus(2, ChronoUnit.HOURS);
         Instant end = Instant.now().plus(4, ChronoUnit.HOURS);
@@ -46,8 +47,9 @@ public class SampleAllocatorChecks {
         assertThat(schedules).isEmpty();
     }
 
-    @Test
-    public void givenAMixOfFittingAndNonFittingTasks_whenAllocating_shouldAllocateFittingTasks() {
+    @ParameterizedTest
+    @MethodSource("generateAllocators")
+    public void givenAMixOfFittingAndNonFittingTasks_whenAllocating_shouldAllocateFittingTasks(Allocator allocator) {
         Task aTask = Task.builder().estimate(Duration.ofHours(5)).build();
         Task bTask = Task.builder().estimate(Duration.ofHours(2)).build();
         Instant start = Instant.now().plus(2, ChronoUnit.HOURS);
